986f28901b226e7710d1db4b06515f2e218e7c98
Unknown <weisenny@gmail.com>
2018-12-15 17:25:31 +0800

    update pages

--- pages/pages.py
+++ pages/pages.py
@@ -5,21 +5,23 @@ import random
 def trans(str1):
 
     num = int(str1)
-    return transtable[num]
+    return pagetable[num]
 
 pagelength = input("Enter length of page (1~7): ")
 pagelength = int(pagelength)
 ranges = pow(10, pagelength)
-transtable = [str(i).zfill(pagelength) for i in range(ranges)]
-random.shuffle(transtable)
+pagetable = [str(i).zfill(pagelength) for i in range(ranges)]
+random.shuffle(pagetable)
 
-
-strs = raw_input("Enter your input address 00000000-99999999: ")
-while strs != "exit":
-
-    str2=trans(strs[0:pagelength])
-    print(str2+strs[pagelength:8])
-    strs = raw_input("Enter your input address 00000000-99999999: ")
+logicaladdress = raw_input("Enter your input address 00000000-99999999: ")
+while logicaladdress != "exit":
+    pagenumber = logicaladdress[0:pagelength]
+    offestaddress = logicaladdress[pagelength:8]
+    blocknumber=trans(pagenumber)
+    physicalregister = offestaddress
+    realaddress=blocknumber+physicalregister
+    print("Physical address is "+realaddress)
+    logicaladdress = raw_input("Enter your input address 00000000-99999999: ")
 
 
 
